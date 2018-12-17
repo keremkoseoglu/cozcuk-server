2e2e0201cbc9281eec9aec00f71b50a94c69110f
Kerem Koseoglu <kerem@koseoglu.info>
2018-12-17 13:01:51 +0300

    Regular update

--- cozweb/http.py
+++ cozweb/http.py
@@ -95,4 +95,4 @@ def is_oauth_valid(user_token: str, app_token: str) -> bool:
     url = "http://graph.facebook.com/debug_token?input_token={0}&access_token={1}".format(user_token, app_token)
     resp = requests.get(url)
     json = resp.json()
-    return str(json["data"]["is_valid"]) == "true"
+    return str(json["data"]["is_valid"]).lower() == "true"
